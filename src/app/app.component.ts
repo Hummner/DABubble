c349e8302b52081e8a133277234fc06d69b1891d
Hummner <bence.cservenyak@gmail.com>
2025-07-02 19:05:06 +0200

    firebase Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Firestore } from '@angular/fire/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,47 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dabubble';
+  firestore!: Firestore;
+  firebaseServer?: () => void;
+
+  constructor() {
+
+    this.firestore = inject(Firestore);
+    console.log(this.firestore);
+    
+    // const testColl = collection(this.firestore, 'Test');
+
+
+  //       this.unsubList = onSnapshot(this.getUserRef(), (list) => {
+  //     this.users = [];
+  //     list.forEach((element) => {
+  //       this.users.push(this.setUserObject(element.data(), element.id));
+  //       console.log(this.users);
+  //     });
+  //   });
+  // }
+
+  }
+
+  ngOnInit(): void {
+    this.testFire();
+  }
+
+  testFire() {
+    this.firebaseServer = onSnapshot(collection(this.firestore, 'Test'), (e) => {
+      e.forEach(el => console.log(el.data()))
+      
+    })
+  }
+
+  ngOnDestroy() {
+    this.firebaseServer;
+  }
+
+
+
+
+
 }
