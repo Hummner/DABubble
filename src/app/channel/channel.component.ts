b6f8fa67836d2370040bf73ed1d3d868353395ca
Hummner <bence.cservenyak@gmail.com>
2025-07-04 07:49:41 +0200

    Channel Component HTML and CSS Added

---
+++ src/app/channel/channel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-channel',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './channel.component.html',
+  styleUrl: './channel.component.scss'
+})
+export class ChannelComponent {
+
+}
