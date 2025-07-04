2ada081339f36ab7ca9f2adf13b2f73cb110d05e
Dora Telekesi <dorothea0929@gmail.com>
2025-07-04 18:56:45 +0200

    Login and Signup centered, header conflict solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,20 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { collection, onSnapshot, Firestore } from '@angular/fire/firestore';
-import { HeaderComponent } from "./shared/header/header.component";
+import { HeaderComponent } from './shared/header/header.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dabubble';
-
-
+  router = inject(Router);
+  constructor() {
+    console.log(this.router.url);
+  }
 }
