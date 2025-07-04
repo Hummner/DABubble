c842ab7100878578b3ddf25c8c1d2847543d4dd5
Dora Telekesi <dorothea0929@gmail.com>
2025-07-04 13:08:42 +0200

    Signup HTML, CSS added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ChannelComponent } from './channel/channel.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 export const routes: Routes = [
-    {path: "channel", component: ChannelComponent},
-    {path:"", component:LoginComponent}
+  { path: 'channel', component: ChannelComponent },
+  { path: '', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
