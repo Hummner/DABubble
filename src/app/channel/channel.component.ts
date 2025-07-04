98b46ff0d70abc493da3cdb3988a7d3f0bf2773b
Hummner <bence.cservenyak@gmail.com>
2025-07-04 16:15:19 +0200

    sidenav added

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatSidenavModule],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss'
 })
 export class ChannelComponent {
 
+  showFiller = false;
 }
