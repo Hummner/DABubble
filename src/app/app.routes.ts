1e3df9bb8dbd0aaabe3e3ddebb4b5ca9f7c38acc
Dora Telekesi <dorothea0929@gmail.com>
2025-07-04 11:59:07 +0200

    Login HTML CSS added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ChannelComponent } from './channel/channel.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
     {path: "channel", component: ChannelComponent},
+    {path:"", component:LoginComponent}
 ];
