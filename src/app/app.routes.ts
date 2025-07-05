666e2978c01a6edece3aaf0e15208ed666b618d2
Dora Telekesi <dorothea0929@gmail.com>
2025-07-05 13:31:26 +0200

    avatar-selection design done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ChannelComponent } from './channel/channel.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AvatarSelectionComponent } from './auth/avatar-selection/avatar-selection.component';
 
 export const routes: Routes = [
   { path: 'channel', component: ChannelComponent },
   { path: '', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'avatarSelection', component: AvatarSelectionComponent },
 ];
