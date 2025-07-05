0927e043f405d4ad5b4ee0af17f2206144bbce00
Hummner <bence.cservenyak@gmail.com>
2025-07-05 20:58:57 +0200

    new Ticket Component

---
+++ src/app/dialogs/show-members/show-members.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-members',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-members.component.html',
+  styleUrl: './show-members.component.scss'
+})
+export class ShowMembersComponent {
+
+}
