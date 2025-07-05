8d8e611850f49e12e3af620d1d80f1702fac4850
Dora Telekesi <dorothea0929@gmail.com>
2025-07-05 14:12:45 +0200

    reset Password 1st page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ChannelComponent } from './channel/channel.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AvatarSelectionComponent } from './auth/avatar-selection/avatar-selection.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 
 export const routes: Routes = [
   { path: 'channel', component: ChannelComponent },
   { path: '', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'avatarSelection', component: AvatarSelectionComponent },
+  { path: 'resetPassword', component: ResetPasswordComponent },
 ];
