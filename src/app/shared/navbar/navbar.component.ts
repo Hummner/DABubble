6bc8920c941c1b053c69a1faa5cd4946abbf1b46
Stefan Redl <bernhardlaus@gmx.at>
2025-07-07 23:35:16 +0200

    continue with the navbar design

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ MatSidenavModule, MatIconModule ],
+  imports: [ MatSidenavModule, MatIconModule, MatToolbarModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
-  showFiller = false;
+  showFiller = true;
   isOpen = true;
 
 
