3a376ea9ac717bd1003d995e018db10619b48d25
Stefan Redl <bernhardlaus@gmx.at>
2025-07-08 22:02:37 +0200

    create dialog component to add a new dialog started with styling and the logic

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { DialogComponent } from '../dialog/dialog.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ MatSidenavModule, MatIconModule, MatToolbarModule ],
+  imports: [ MatSidenavModule, MatIconModule, MatToolbarModule, MatDialogModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  constructor(public dialog: MatDialog) {}
+
   showFiller = true;
   isOpen = true;
-
 
   toggleDrawer() {
     this.isOpen = !this.isOpen;
   }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+
 }
