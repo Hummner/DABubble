b4c26664f680e3add8acc2cd6791c5a554f88c1c
Dora Telekesi <dorothea0929@gmail.com>
2025-07-08 12:50:44 +0200

    login and signup validation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -8,7 +8,7 @@ import {
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
-
+import { getAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-signup',
   standalone: true,
@@ -19,19 +19,33 @@ import { Router, RouterLink } from '@angular/router';
 export class SignupComponent {
   authService = inject(AuthService);
   router = inject(Router);
+  auth = getAuth();
+  submitted = false;
+  disabled = true;
 
-  form = new FormGroup({
+  signupForm = new FormGroup({
     name: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
+    privacyPolicy: new FormControl('', Validators.requiredTrue),
   });
 
   errorMessage: string | null = null;
 
   constructor() {}
 
+  get signupFormControl() {
+    return this.signupForm.controls;
+  }
+
+  get isFormEmpty() {
+    const { name, email, password, privacyPolicy } = this.signupForm.value;
+    return !name?.trim() || !email?.trim() || !password?.trim() || !privacyPolicy;
+  }
+
   onSubmit() {
-    const rawForm = this.form.getRawValue();
+    this.submitted = true;
+    const rawForm = this.signupForm.getRawValue();
     this.authService
       .register(rawForm.name!, rawForm.email!, rawForm.password!)
       .subscribe({
@@ -39,7 +53,13 @@ export class SignupComponent {
           this.router.navigateByUrl('/avatarSelection');
         },
         error: (err) => {
-          this.errorMessage = err.code;
+          if (err.code === 'auth/invalid-email') {
+            this.signupForm.get('email')?.setErrors({ invalidEmail: true });
+          } else if (err.code === 'auth/wrong-password') {
+            this.signupForm.get('password')?.setErrors({ incorrect: true });
+          } else if (err.code === 'auth/invalid-credential') {
+            this.signupForm.get('password')?.setErrors({ invalid: true });
+          }
         },
       });
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -33,6 +33,7 @@ export class AuthService {
       email,
       password
     ).then(() => {});
+    ;
     return from(promise);
   }
 
