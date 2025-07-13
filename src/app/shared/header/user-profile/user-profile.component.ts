7a568d8d439b1b534f3d574ea21951bf7ccd62ef
Dora Telekesi <dorothea0929@gmail.com>
2025-07-13 10:30:27 +0200

    user profile edit with avatar selection

--- src/app/shared/header/user-profile/user-profile.component.ts
+++ src/app/shared/header/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FirestoreService } from '../../../services/firestore.service';
@@ -17,9 +17,21 @@ export class UserProfileComponent implements OnInit {
   @Output() close = new EventEmitter();
   userProfile = this.firestoreService.userProfile;
   edit = false;
+  editImg = false;
   user!: UserProfileInterface;
   editableUser!: UserProfileInterface;
-  
+  profileImageUrls = [
+    'assets/img/elias_neumann.svg',
+    'assets/img/elise_roth.svg',
+    'assets/img/frederik_beck.svg',
+    'assets/img/noah_braun.svg',
+    'assets/img/sofia_müller.svg',
+    'assets/img/steffen_hoffmann.svg',
+  ];
+  currentProfilImageUrl = '';
+  profileImageChosen = false;
+  showTooltip = false;
+
   ngOnInit() {
     const user = this.userProfile();
     if (user) {
@@ -38,10 +50,25 @@ export class UserProfileComponent implements OnInit {
     this.edit = true;
   }
 
+  openEditProfileImg() {
+    this.editImg = true;
+    this.edit = false;
+  }
+
+  changeProfilImg(src: string) {
+    this.currentProfilImageUrl = src;
+    this.profileImageChosen = true;
+  }
+
   closeEdit() {
     this.user = { ...this.editableUser };
     this.saveName();
     this.edit = false;
+  }
+
+  closeEditImg() {
+    this.editImg = false;
+    this.saveImg();
   }
 
   cancelEdit() {
@@ -49,6 +76,12 @@ export class UserProfileComponent implements OnInit {
   }
 
   saveName() {
+    this.firestoreService.updateUser(this.user);
+  }
+
+  saveImg() {
+    this.editableUser.imgUrl = this.currentProfilImageUrl;
+    this.user = { ...this.editableUser };
     this.firestoreService.updateUser(this.user);
   }
 }
