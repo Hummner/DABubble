9482afaa09ab643166f9c64ec25cb4769bce6fdc
Stefan Redl <bernhardlaus@gmx.at>
2025-07-14 20:16:38 +0200

    all channels are retrieved from the Firestore and updated in the navbar.component

---
+++ src/app/interfaces/navbar.interface.ts
@@ -0,0 +1,4 @@
+export interface NavbarInterface {
+    id: string,
+    name:string,
+}
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,34 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { NewChannelComponent } from './new-channel/new-channel.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { NewChannelComponent } from './new-channel/new-channel.component';
+import { AddChannelMemberComponent } from './add-channel-member/add-channel-member.component';
+import { NavbarService } from '../../services/navbar.service';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ MatSidenavModule, MatIconModule, MatToolbarModule, MatDialogModule ],
+  imports: [
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDialogModule,
+    NgFor,
+    NgIf,
+    AsyncPipe
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  private dialog = inject(MatDialog);
+  channels$ = inject(NavbarService).channelsObs$;   // <<—  Daten‑Stream
 
-  constructor(public dialog: MatDialog) {}
-
+  isOpen = true;
   showChannel = true;
   showMessage = true;
-  isOpen = true;
 
   toggleDrawer() {
     this.isOpen = !this.isOpen;
   }
 
   openDialog() {
-    const dialogRef = this.dialog.open(NewChannelComponent);
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
+    this.dialog.open(NewChannelComponent)
+      .afterClosed()
+      .subscribe(channelName => {
+        if (!channelName) return;
+        this.dialog.open(AddChannelMemberComponent, {
+          data: { channelName }
+        });
+      });
   }
 }
