40f6e40f88e580b3f23727d4537bc34c80339990
Dora Telekesi <dorothea0929@gmail.com>
2025-07-24 14:39:36 +0200

    avatar-selection and password reset pages responsively designed; header-2 adjusted; layout of avatar selection corrected

--- src/app/shared/header-2/header-2.component.ts
+++ src/app/shared/header-2/header-2.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header-2',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './header-2.component.html',
-  styleUrl: './header-2.component.scss'
+  styleUrl: './header-2.component.scss',
 })
 export class Header2Component {
+  isLoginPage = false;
+  router = inject(Router);
 
+  constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects;
+        this.isLoginPage = url === '/';
+      }
+    });
+  }
 }
