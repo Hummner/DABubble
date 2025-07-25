ba24601b96e09d41cc27a24f41430d42a9ae8819
Hummner <bence.cservenyak@gmail.com>
2025-07-25 21:20:56 +0200

    add Emoji to Ticket with Firestore

--- src/app/shared/messages/ticket/ticket.component.ts
+++ src/app/shared/messages/ticket/ticket.component.ts
@@ -62,28 +62,77 @@ export class TicketComponent implements OnInit, OnChanges {
   }
 
   async addEmojiToTicket(emoji: string) {
-    let ticketRef = this.getTicketRef();
     let senderId = this.getCurrentUserId();
     let isEmoji: boolean = this.checkEmojiInArray(emoji);
-    let isUserAddedReaction: boolean = this.checkUserReactions(senderId!)
+    let isUserAddedReaction: boolean = this.checkUserReactions(senderId!);
+    let reactionsCopy: { emoji: string; users: string[] }[] = [...this.ticket.reactions];
+    let indexOfEmoji = this.getIndexOfEmoji(emoji)
 
-    if (isEmoji && isUserAddedReaction) {
-      console.log("This user yet added This emoji:", emoji);
+    if (isEmoji && !isUserAddedReaction) {
+      reactionsCopy[indexOfEmoji] = this.addUserIdToEmoji(senderId!, indexOfEmoji, emoji, reactionsCopy)
+      this.updateReaction(reactionsCopy)
+    } else if (!isEmoji) {
+      this.addnewEmoji(reactionsCopy, emoji, senderId!)
+      this.updateReaction(reactionsCopy)
 
+    } else if (isEmoji && isUserAddedReaction) {
+      this.deleteUserOrEmoji(reactionsCopy, indexOfEmoji, senderId!, emoji)
     }
+  }
 
-    if (isEmoji && isUserAddedReaction) {
-      
+  deleteUserOrEmoji(reactionsCopy: { emoji: string; users: string[] }[], indexOfEmoji: number, senderId: string, emoji: string) {
+    let users = reactionsCopy[indexOfEmoji].users
+    let indexUser = reactionsCopy[indexOfEmoji].users.findIndex(user => user === senderId!)
+    let newUserArray = users.splice(indexUser, 1)
+
+    if (users.length === 0) {
+      reactionsCopy.splice(indexOfEmoji, 1);
+      this.updateReaction(reactionsCopy)
+    } else {
+      reactionsCopy[indexOfEmoji] = {
+        emoji: emoji,
+        users: newUserArray
+      }
+      this.updateReaction(reactionsCopy)
     }
+  }
+
+  addnewEmoji(reactionsCopy: { emoji: string; users: string[] }[], emoji: string, senderId: string) {
+    reactionsCopy.push({
+      emoji: emoji,
+      users: [senderId!]
+    })
+  }
+
+  addUserIdToEmoji(senderId: string, indexOfEmoji: number, emoji: string, reactionsCopy: { emoji: string; users: string[] }[]) {
+    let usersCopy = [...this.ticket.reactions[indexOfEmoji].users]
+    usersCopy.push(senderId!)
+
+    return reactionsCopy[indexOfEmoji] = {
+      emoji: emoji,
+      users: usersCopy
+    }
+  }
 
 
+  async updateReaction(reactionsCopy: { emoji: string; users: string[] }[]) {
+    let ticketRef = this.getTicketRef();
     try {
-      await updateDoc(ticketRef, "reactions", arrayUnion())
+      await updateDoc(ticketRef, {
+        reactions: reactionsCopy
+      });
+      this.ticket.reactions = reactionsCopy;
     } catch (err) {
-      console.error("Error by add a reaction", err);
+      console.error("Failed to update reactions:", err);
 
     }
   }
+
+  getIndexOfEmoji(emoji: string) {
+    return this.ticket.reactions.findIndex(reaction => reaction.emoji === emoji);
+  }
+
+
 
   checkUserReactions(senderId: string) {
     let isUserAddedReaction = false;
@@ -173,7 +222,9 @@ export class TicketComponent implements OnInit, OnChanges {
   }
 
   getTicketRef() {
-    return doc(this.channelService.firestore, this.threadsService.getTicketPath())
+    let threadPath = this.ticket.threads?.path;
+    let ticketPath = threadPath?.split('/').slice(0, 4).join('/')
+    return doc(this.channelService.firestore, ticketPath!)
   }
 
 
