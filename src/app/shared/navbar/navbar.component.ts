2f6b23c2da391475db3038fd516069679c03d207
Dora Telekesi <dorothea0929@gmail.com>
2025-08-04 22:58:43 +0200

    Fix: navbar issue after rebase onto develop

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject,signal,computed } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -26,7 +26,7 @@ import { ChannelsService } from '../../services/channels.service';
     NgIf,
     AsyncPipe,
     RouterModule,
-    NgClass
+    NgClass,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
@@ -38,18 +38,17 @@ export class NavbarComponent {
   readonly channelUsers = this.directMessageService.userIds;
   readonly currentUserId = computed(() => this.userProfile()?.uid);
   readonly selectedUserId = computed(() => {
-  const users = this.channelUsers(); 
-  const current = this.currentUserId();
-  if (!users || !current) return null;
-  return users.find(uid => uid !== current) ?? null;
-});
+    const users = this.channelUsers();
+    const current = this.currentUserId();
+    if (!users || !current) return null;
+    return users.find((uid) => uid !== current) ?? null;
+  });
 
   constructor(
     public dialog: MatDialog,
     private firestoreService: FirestoreService,
     private directMessageService: DirectMessageService,
     private router: Router
-    
   ) {}
 
   isOpen = true;
@@ -72,10 +71,10 @@ export class NavbarComponent {
       });
   }
 
- getOtherUserList(): UserProfileInterface[] {
-  const users = this.firestoreService.userList(); // call to get the current value
-  return users.filter(user => user.uid !== this.userProfile()?.uid);
-}
+  getOtherUserList(): UserProfileInterface[] {
+    const users = this.firestoreService.userList(); // call to get the current value
+    return users.filter((user) => user.uid !== this.userProfile()?.uid);
+  }
 
   async findOrCreateDMchannel(currentUserId: string, clickedUserId: string) {
     const channelId = await this.directMessageService.getDMChannel(
@@ -83,19 +82,20 @@ export class NavbarComponent {
       clickedUserId
     );
     this.router.navigateByUrl(`directMessages/${channelId}`);
-        this.directMessageService.subDMChannel(channelId);
+    this.directMessageService.subDMChannel(channelId);
   }
- 
-  selectChannel(channelId: string) {
 
+  selectChannel(channelId: string) {
     this.channels$.subscribe((channels) => {
-      const selectedChannel = channels.find(channel => channel.channelId === channelId);
+      const selectedChannel = channels.find(
+        (channel) => channel.channelId === channelId
+      );
       if (selectedChannel) {
         this.channelService.getChannel(selectedChannel.channelId);
         this.router.navigateByUrl(`channel/${selectedChannel.channelId}`);
       } else {
         console.error('Channel not found:', channelId);
       }
-  });
+    });
   }
 }
