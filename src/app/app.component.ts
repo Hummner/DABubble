3dca7d5268e6e26dd6f4068c1e458693a5625196
Dora Telekesi <dorothea0929@gmail.com>
2025-08-22 13:21:40 +0200

    Revert "feat: dashboard component created; mobile navigation between routes added"
    This reverts commit 288bc7182fffbf96dc283d93ca730ed9b7978fa9.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,19 +18,31 @@ export class AppComponent {
   title = 'dabubble';
   router = inject(Router);
   isAuthLayout = false;
+    isNavbarClosed = false;
   constructor() {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         const url = event.urlAfterRedirects;
         this.isAuthLayout =
           url === '/' ||
-          [
-            '/resetPassword',
-            '/signup',
-            '/avatarSelection',
-            '/resetPassword/newPassword',
-          ].some((path) => url.startsWith(path));
+          ['/resetPassword', '/signup', '/avatarSelection', '/resetPassword/newPassword'].some((path) => url.startsWith(path));
+        const hideNavbarRoutes = ['/channel/', '/directMessages/', '/newMessage'];
+        this.isNavbarClosed = hideNavbarRoutes.some((path) => url.startsWith(path)) && this.windowWidth < 992;
+
+        console.log('URL:', url, 'Navbar closed:', this.isNavbarClosed, 'Width:', this.windowWidth);
       }
     });
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.windowWidth = (event.target as Window).innerWidth;
+    const url = this.router.url;
+    const hideNavbarRoutes = ['/channel/', '/directMessages/', '/newMessage'];
+    this.isNavbarClosed = hideNavbarRoutes.some((path) => url.startsWith(path)) && this.windowWidth < 992;
+  }
+
+  onToggleNavbar() {
+    this.isNavbarClosed = false;
+    this.router.navigateByUrl('/channel/1aJzYjqviVDIhmPzxmtc');
   }
 }
