397c80e3c3f5a58487ae099bc76499cb5be643a9
Dora Telekesi <dorothea0929@gmail.com>
2025-11-10 08:05:03 +0100

    fix: correct navigation to - from impressum and privacy policy

--- src/app/datenschutz/datenschutz.component.ts
+++ src/app/datenschutz/datenschutz.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Header2Component } from '../shared/header-2/header-2.component';
 import { Router, RouterLink } from '@angular/router';
+import { NavigationService } from '../services/navigation.service';
 
 @Component({
   selector: 'app-datenschutz',
@@ -9,4 +10,17 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './datenschutz.component.html',
   styleUrl: './datenschutz.component.scss',
 })
-export class DatenschutzComponent {}
+export class DatenschutzComponent implements OnInit {
+  prevUrl!: string | null;
+
+  constructor(private navigationService: NavigationService) {}
+  ngOnInit() {
+    this.prevUrl = this.navigationService.getPreviousUrl();
+    if (this.prevUrl == null || this.prevUrl == '/') {
+      this.prevUrl = '/';
+    }
+    if (this.prevUrl == '/signup') {
+      this.prevUrl = '/signup';
+    }
+  }
+}
--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Header2Component } from '../shared/header-2/header-2.component';
 import { Router, RouterLink } from '@angular/router';
-
+import { NavigationService } from '../services/navigation.service';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
   imports: [Header2Component, RouterLink],
   templateUrl: './impressum.component.html',
-  styleUrl: './impressum.component.scss'
+  styleUrl: './impressum.component.scss',
 })
-export class ImpressumComponent {
+export class ImpressumComponent implements OnInit {
+  prevUrl!: string | null;
 
+  constructor(private navigationService: NavigationService) {}
+  ngOnInit() {
+    this.prevUrl = this.navigationService.getPreviousUrl();
+    if (this.prevUrl == null || this.prevUrl == '/') {
+      this.prevUrl = '/';
+    }
+    if (this.prevUrl == '/signup') {
+      this.prevUrl = '/signup';
+    }
+  }
 }
